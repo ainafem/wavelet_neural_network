188bed14618965bf503e34fea620cc38abe94e1b
Aliarachan <kaala_llunaa@hotmail.com>
2018-03-21 21:22:23 +0100

    successful wavelet layer

--- WaveletLayer.py
+++ WaveletLayer.py
@@ -4,51 +4,60 @@ import numpy as np
 
 class MyLayer(Layer):
 
-    def __init__(self, output_dim, haar_matrix, **kwargs):
-        self.output_dim = output_dim
+    def __init__(self, output_dim, haar_matrix, batch_size, **kwargs):
         self.haarMatrix = haar_matrix
+        self.batch_size = batch_size
+        self.output_dim = output_dim
         super(MyLayer, self).__init__(**kwargs)
 
     def dwt(self, image):
         if not (image.shape[0] % 4 == 0) or not (image.shape[1] % 4 == 0):
-            print("The size of your image must bea multiple of 4!")
-            return None
+            print(K.int_shape(image))
+            print("The size of your image must be a multiple of 4!")
 
-        haarMatrix2 = K.transpose(self.haarMatrix)
-        result = K.zeros(image.shape, dtype=K.float)
+        haarMatrix1 = K.variable(self.haarMatrix, dtype="float32")
+        haarMatrix2 = K.repeat_elements(K.expand_dims(K.transpose(haarMatrix1), 0), int(K.get_value(self.batch_size)), 0)
+        haarMatrix1 = K.repeat_elements(K.expand_dims(haarMatrix1, 0), int(K.get_value(self.batch_size)), 0)
+        result = []
         nchannels = 3
         if len(image.shape) == nchannels:
-            data0 = image[:, :, 0]
+            data0 = image[:, :, :, 0]
             result0 = K.batch_dot(K.batch_dot(self.haarMatrix, data0), self.haarMatrix2)
-            result[:, :, 0] = result0
+            result[:, :, :, 0] = result0
 
-            data1 = image[:, :, 1]
-            result1 = K.batch_dot(K.batch_dot(self.haarMatrix, data0), self.haarMatrix2)
-            result[:, :, 1] = result1
+            data1 = image[:, :, :, 1]
+            result1 = K.batch_dot(K.batch_dot(self.haarMatrix, data1), self.haarMatrix2)
+            result[:, :, :, 1] = result1
 
-            data2 = image[:, :, 2]
-            result2 = K.batch_dot(K.batch_dot(self.haarMatrix, data0), self.haarMatrix2)
-            result[:, :, 2] = result2
+            data2 = image[:, :, :, 2]
+            result2 = K.batch_dot(K.batch_dot(self.haarMatrix, data2), self.haarMatrix2)
+            result[:, :, :, 2] = result2
 
         else:
+            nconv = K.int_shape(image)[-1]
+            for i in range(nconv):
+                tmp = K.batch_dot(haarMatrix1, image[:,:,:,i], axes=[1, 2])
+                print(K.int_shape(tmp))
+                result.append(K.batch_dot(tmp, haarMatrix2, axes=[1,2]))
 
-            result = K.batch_dot(K.batch_dot(self.haarMatrix, image), haarMatrix2)
+        result = K.concatenate(result, -1)
+        result = K.reshape(result, (-1, K.int_shape(image)[1], K.int_shape(image)[2], nconv))
 
         return result
 
 
     def build(self, input_shape):
         # Create a trainable weight variable for this layer.
-        self.kernel = self.add_weight(name='kernel',
-                                      shape=(input_shape[1], self.output_dim),
-                                      initializer='uniform',
-                                      trainable=True)
+        #self.kernel = self.add_weight(name='kernel',shape=(input_shape[1], self.output_dim),initializer='uniform',trainable=True)
         super(MyLayer, self).build(input_shape)  # Be sure to call this somewhere!
 
 
     def call(self, x):
+
         result = self.dwt(x)
-        return result[:K.cast(K.int_shape(result)[0]/2, dtype="int"), :K.cast(K.int_shape(result)[1]/2, dtype= "int")]
+        result = result[:, :int(K.int_shape(x)[1] / 2), : int(K.int_shape(x)[2]/2) , :]
+        print(K.int_shape(result))
+        return result
 
     def compute_output_shape(self, input_shape):
-        return (input_shape[0], self.output_dim)
+        return self.output_dim
--- test.py
+++ test.py
@@ -8,6 +8,7 @@ from keras import Model, Sequential
 from keras.datasets import mnist
 from keras.utils import np_utils
 from wavelet_neural_network import WaveletLayer as wl
+from update_batch_size import update_batch_size
 
 from keras import backend as K
 import matplotlib.image as mpimg
@@ -33,7 +34,7 @@ def build_haar_matrix(row_size):
     m = np.concatenate((haarM1, haarM2), axis=0)
     return m
 
-
+batch_size = K.variable(32)
 
 haarMatrix28 = build_haar_matrix(28)
 haarMatrix14 = build_haar_matrix(14)
@@ -54,14 +55,19 @@ y_test = np_utils.to_categorical(y_test, number_of_classes)
 print(y_train)
 
 input = Input(shape=(28, 28, 1))
-conv1 = Conv2D(20, kernel_size=(5,5))(input)
+conv1 = Conv2D(20, kernel_size=(5,5), padding='same')(input)
 batch1 = BatchNormalization()(conv1)
 #pool1 = MaxPooling2D(pool_size=(2, 2))(batch1)
-pool1 = wl.MyLayer(output_dim=(14, 14, 1), haar_matrix=haarMatrix28)(batch1)
-conv2 = Conv2D(50, kernel_size=(5,5))(pool1)
+pool1 = wl.MyLayer(output_dim=(None, 14, 14, 20), batch_size=batch_size, haar_matrix=haarMatrix28)(batch1)
+
+conv2 = Conv2D(50, kernel_size=(5,5), padding="same")(pool1)
+
 #pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-pool2 = wl.MyLayer(output_dim=(7, 7), haar_matrix=haarMatrix14)(conv2)
+
+pool2 = wl.MyLayer(output_dim=(None, 7, 7, 50), batch_size=batch_size, haar_matrix=haarMatrix14)(conv2)
+
 batch2 = BatchNormalization()(pool2)
+
 conv3 = Conv2D(500, kernel_size=(4,4))(batch2)
 relu = LeakyReLU(alpha=0)(conv3)
 conv4 = Conv2D(10, kernel_size=(1, 1))(relu)
@@ -73,17 +79,11 @@ model = Model(inputs=input, outputs=activ)
 model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
 
-model.fit(x_train, y_train, epochs=5, batch_size=32)
+model.fit(x_train, y_train, epochs=5, batch_size=32, callbacks=[update_batch_size(batch_size, x_train.shape[0])])
 
+K.set_value(batch_size, 128)
 
-# 9. Fit model on training data
-# model.fit(x_train, y_train,
-#           batch_size=32, nb_epoch=10, verbose=1)
-
-# 10. Evaluate model on test data
-#score = model.evaluate(x_test, y, verbose=0)
-
-loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
+loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128, callbacks=[update_batch_size(batch_size, x_test.shape[0])])
 
 
 
