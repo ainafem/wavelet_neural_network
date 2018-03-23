1fe583b87a6bf243ca3c3140b49f276c655a08f7
EduardoAguilar <eaaguilart@gmail.com>
2018-03-23 07:48:39 +0100

    it adapted the code to keras 1.2 + theano, and the wavelet layer was modified to accept a dynamic batch_size.

--- WaveletLayer.py
+++ WaveletLayer.py
@@ -1,63 +1,45 @@
 from keras import backend as K
 from keras.engine.topology import Layer
-import numpy as np
 
 class MyLayer(Layer):
 
-    def __init__(self, output_dim, haar_matrix, batch_size, **kwargs):
+    def __init__(self, output_dim, haar_matrix, **kwargs):
         self.haarMatrix = haar_matrix
-        self.batch_size = batch_size
         self.output_dim = output_dim
         super(MyLayer, self).__init__(**kwargs)
 
     def dwt(self, image):
+        # image.shape = [None, nconv, width, height]
         if not (image.shape[0] % 4 == 0) or not (image.shape[1] % 4 == 0):
             print(K.int_shape(image))
             print("The size of your image must be a multiple of 4!")
 
         haarMatrix1 = K.variable(self.haarMatrix, dtype="float32")
-        haarMatrix2 = K.repeat_elements(K.expand_dims(K.transpose(haarMatrix1), 0), int(K.get_value(self.batch_size)), 0)
-        haarMatrix1 = K.repeat_elements(K.expand_dims(haarMatrix1, 0), int(K.get_value(self.batch_size)), 0)
+        input_ones_matrix= (K.abs(image)+1)/(K.abs(image)+1)
+        haarMatrix2 = input_ones_matrix * K.transpose(haarMatrix1)
+        haarMatrix1 = input_ones_matrix * haarMatrix1
+
         result = []
-        nchannels = 3
-        if len(image.shape) == nchannels:
-            data0 = image[:, :, :, 0]
-            result0 = K.batch_dot(K.batch_dot(self.haarMatrix, data0), self.haarMatrix2)
-            result[:, :, :, 0] = result0
 
-            data1 = image[:, :, :, 1]
-            result1 = K.batch_dot(K.batch_dot(self.haarMatrix, data1), self.haarMatrix2)
-            result[:, :, :, 1] = result1
-
-            data2 = image[:, :, :, 2]
-            result2 = K.batch_dot(K.batch_dot(self.haarMatrix, data2), self.haarMatrix2)
-            result[:, :, :, 2] = result2
-
-        else:
-            nconv = K.int_shape(image)[-1]
-            for i in range(nconv):
-                tmp = K.batch_dot(haarMatrix1, image[:,:,:,i], axes=[1, 2])
-                print(K.int_shape(tmp))
-                result.append(K.batch_dot(tmp, haarMatrix2, axes=[1,2]))
+        nconv = K.int_shape(image)[1]
+        for i in range(nconv):
+            tmp = K.batch_dot(haarMatrix1[:,i,:,:], image[:,i,:,:], axes=[1, 2])
+            result.append(K.batch_dot(tmp, haarMatrix2[:,i,:,:], axes=[1,2]))
 
         result = K.concatenate(result, -1)
-        result = K.reshape(result, (-1, K.int_shape(image)[1], K.int_shape(image)[2], nconv))
+        result = K.reshape(result, (-1, nconv, K.int_shape(image)[2], K.int_shape(image)[3]))
 
         return result
 
 
     def build(self, input_shape):
-        # Create a trainable weight variable for this layer.
-        #self.kernel = self.add_weight(name='kernel',shape=(input_shape[1], self.output_dim),initializer='uniform',trainable=True)
         super(MyLayer, self).build(input_shape)  # Be sure to call this somewhere!
 
 
-    def call(self, x):
-
+    def call(self, x, mask=None):
         result = self.dwt(x)
-        result = result[:, :int(K.int_shape(x)[1] / 2), : int(K.int_shape(x)[2]/2) , :]
-        print(K.int_shape(result))
+        result = result[:, :, :int(K.int_shape(x)[2] / 2), : int(K.int_shape(x)[3]/2)]
         return result
 
-    def compute_output_shape(self, input_shape):
+    def get_output_shape_for(self, input_shape):
         return self.output_dim
--- test.py
+++ test.py
@@ -1,23 +1,19 @@
-from keras.layers import Conv2D, MaxPooling2D, Input, BatchNormalization, Activation, Softmax, LeakyReLU
+'''
+Keras 1.2 + Theano.
+'''
+import os
+os.environ['THEANO_FLAGS'] = "device=cuda0,floatX=float32,optimizer=fast_compile,optimizer_including=fusion, dnn.enabled=False"  
+os.environ['CPLUS_INCLUDE_PATH'] = '/usr/local/cuda/include'
 
-import math
-
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
-from keras import Model, Sequential
+from keras.layers import Conv2D, Input, BatchNormalization, LeakyReLU, Dense, Flatten
+from keras.models import Model
 from keras.datasets import mnist
 from keras.utils import np_utils
-from wavelet_neural_network import WaveletLayer as wl
-from update_batch_size import update_batch_size
 
-from keras import backend as K
-import matplotlib.image as mpimg
+import WaveletLayer as wl
+
+import math
 import numpy as np
-import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
 
 def build_haar_matrix(row_size):
     size = (int)(row_size / 2)
@@ -34,15 +30,13 @@ def build_haar_matrix(row_size):
     m = np.concatenate((haarM1, haarM2), axis=0)
     return m
 
-batch_size = K.variable(32)
-
 haarMatrix28 = build_haar_matrix(28)
 haarMatrix14 = build_haar_matrix(14)
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-x_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
-x_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+x_train = X_train.reshape(X_train.shape[0], 1, 28, 28)
+x_test = X_test.reshape(X_test.shape[0], 1, 28, 28)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
@@ -54,36 +48,32 @@ y_test = np_utils.to_categorical(y_test, number_of_classes)
 
 print(y_train)
 
-input = Input(shape=(28, 28, 1))
-conv1 = Conv2D(20, kernel_size=(5,5), padding='same')(input)
+input = Input(shape=(1, 28, 28))
+conv1 = Conv2D(20, 5,5, border_mode='same')(input)
 batch1 = BatchNormalization()(conv1)
-#pool1 = MaxPooling2D(pool_size=(2, 2))(batch1)
-pool1 = wl.MyLayer(output_dim=(None, 14, 14, 20), batch_size=batch_size, haar_matrix=haarMatrix28)(batch1)
 
-conv2 = Conv2D(50, kernel_size=(5,5), padding="same")(pool1)
+pool1 = wl.MyLayer(output_dim=(None, 20, 14, 14),  haar_matrix=haarMatrix28)(batch1)
 
-#pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+conv2 = Conv2D(50,5,5,  border_mode='same')(pool1)
 
-pool2 = wl.MyLayer(output_dim=(None, 7, 7, 50), batch_size=batch_size, haar_matrix=haarMatrix14)(conv2)
+pool2 = wl.MyLayer(output_dim=(None, 50, 7, 7),  haar_matrix=haarMatrix14)(conv2)
 
 batch2 = BatchNormalization()(pool2)
 
-conv3 = Conv2D(500, kernel_size=(4,4))(batch2)
+conv3 = Conv2D(500,4,4)(batch2)
 relu = LeakyReLU(alpha=0)(conv3)
-conv4 = Conv2D(10, kernel_size=(1, 1))(relu)
+conv4 = Conv2D(10, 1, 1)(relu)
 batch3 = BatchNormalization()(conv4)
 flat = Flatten()(batch3)
-activ = Dense(units=10, activation='softmax')(flat)
-model = Model(inputs=input, outputs=activ)
+activ = Dense(10, activation='softmax')(flat)
+model = Model(input, activ)
 
 model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
 
-model.fit(x_train, y_train, epochs=5, batch_size=32, callbacks=[update_batch_size(batch_size, x_train.shape[0])])
+model.fit(x_train, y_train, nb_epoch=5, batch_size=32)
 
-K.set_value(batch_size, 128)
-
-loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128, callbacks=[update_batch_size(batch_size, x_test.shape[0])])
+loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
 
 
 
